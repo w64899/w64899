319dcb420e97515dba19fb50345b1e68628e6f61
w64899 <eleonora.krab@gmail.com>
2023-06-16 20:00:40 +0200

    Prepared navigation structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-import './App.css';
+import {Routes, Route} from "react-router-dom";
+import Men from "./pages/Men";
+import Women from "./pages/Women";
+import Accessories from "./pages/Accessories";
+import Electronics from "./pages/Electronics";
+import Homepage from "./pages/Homepage";
 
 function App() {
   return (
     <div className="App">
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/men" element={<Men />} />
+        <Route path="/women" element={<Women />} />
+        <Route path="/accessories" element={<Accessories />} />
+        <Route path="/electronics" element={<Electronics />} />
+      </Routes>
     </div>
   );
 }
