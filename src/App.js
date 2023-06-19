fa681fcc46689cc43e0e1137981288af331507d1
w64899 <eleonora.krab@gmail.com>
2023-06-19 21:16:47 +0200

    Updated routes

--- src/App.js
+++ src/App.js
@@ -4,16 +4,24 @@ import Women from "./pages/Women";
 import Accessories from "./pages/Accessories";
 import Electronics from "./pages/Electronics";
 import Homepage from "./pages/Homepage";
+import Navigation from "./components/Navigation/Navigation";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import ProductPage from "./pages/ProductPage";
 
 function App() {
   return (
     <div className="App">
+      <Navigation/>
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/men" element={<Men />} />
         <Route path="/women" element={<Women />} />
         <Route path="/accessories" element={<Accessories />} />
         <Route path="/electronics" element={<Electronics />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/product/:productID" element={<ProductPage />} />
       </Routes>
     </div>
   );
